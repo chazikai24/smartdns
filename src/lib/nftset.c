7189a490ed35fe84dc51bb63fe802919ac34f417
chazikai24 <kayyes@hotmail.com>
2025-06-21 14:45:48 +0800

    Update nftset.c
    add tlog

--- src/lib/nftset.c
+++ src/lib/nftset.c
@@ -591,10 +591,10 @@ int nftset_flush_cache(void) {
         _nftset_add_element(nffamily, entry->tablename, entry->setname, entry->addr, entry->addr_len,
                               addr_end, addr_end_len, timeout, next, &next);
 	if (entry->addr_len == DNS_RR_A_LEN) {
-	    tlog(TLOG_INFO, "nftset flush cache:%d, family:%s, table:%s, set:%s, IP: %d.%d.%d.%d", i, familyname, tablename, setname, addr[0], addr[1], addr[2],addr[3]);
+	    tlog(TLOG_INFO, "nftset flush cache:%d, family:%s, table:%s, set:%s, IP: %d.%d.%d.%d", i, entry->familyname, entry->tablename, entry->setname, addr[0], addr[1], addr[2],addr[3]);
         } else if (entry->addr_len == DNS_RR_AAAA_LEN) {
 	    tlog(TLOG_INFO, "nftset flush cache:%d, family:%s, table:%s, set:%s, IP: "
-		"%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x", i, familyname, tablename, setname, 
+		"%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x:%.2x%.2x", i, entry->familyname, entry->tablename, entry->setname, 
 	        addr[0], addr[1], addr[2], addr[3], addr[4], addr[5], addr[6], addr[7], addr[8], addr[9], addr[10], addr[11],
 	        addr[12], addr[13], addr[14], addr[15]);
         }
